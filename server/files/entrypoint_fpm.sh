808c41483a73f97161f9613c3dc7765f0551b890
Stefano Ortolani <ortolanis@vmware.com>
2023-05-12 13:42:23 +0100

    Set sessions to be stored in Redis rather than files

--- server/files/entrypoint_fpm.sh
+++ server/files/entrypoint_fpm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ -z "$REDIS_FQDN" ] && REDIS_FQDN=redis
+
 term_proc() {
     echo "Entrypoint FPM caught SIGTERM signal!"
     echo "Killing process $master_pid"
@@ -16,6 +18,8 @@ change_php_vars() {
         sed -i "s/max_execution_time = .*/max_execution_time = 300/" "$FILE"
         sed -i "s/upload_max_filesize = .*/upload_max_filesize = 50M/" "$FILE"
         sed -i "s/post_max_size = .*/post_max_size = 50M/" "$FILE"
+        sed -i "s/session.save_handler = .*/session.save_handler = redis/" "$FILE"
+        sed -i "s|.*session.save_path = .*|session.save_path = 'tcp://${REDIS_FQDN}:6379'|" "$FILE"
     done
 }
 
