3fbf7bff004bcd92544e6cbc90500432b04f80e7
Stefano Ortolani <ortolanis@vmware.com>
2024-08-20 20:12:00 +0100

    Fix default value (again)

--- core/files/entrypoint_fpm.sh
+++ core/files/entrypoint_fpm.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-[ -z "$REDIS_FQDN" ] && REDIS_FQDN=redis
-
 term_proc() {
     echo "Entrypoint FPM caught SIGTERM signal!"
     echo "Killing process $master_pid"
@@ -9,6 +7,9 @@ term_proc() {
 }
 
 trap term_proc SIGTERM
+
+[ -z "$REDIS_FQDN" ] && REDIS_FQDN=redis
+[ -z "$REDIS_PASSWORD" ] && REDIS_PASSWORD=redispassword
 
 change_php_vars() {
     for FILE in /etc/php/*/fpm/php.ini
@@ -19,7 +20,7 @@ change_php_vars() {
         sed -i "s/upload_max_filesize = .*/upload_max_filesize = 50M/" "$FILE"
         sed -i "s/post_max_size = .*/post_max_size = 50M/" "$FILE"
         sed -i "s/session.save_handler = .*/session.save_handler = redis/" "$FILE"
-        sed -i "s|.*session.save_path = .*|session.save_path = '$(echo $REDIS_FQDN | grep -E '^\w+://' || echo tcp://$REDIS_FQDN):6379${REDIS_PASSWORD:+?auth=${REDIS_PASSWORD}}'|" "$FILE"
+        sed -i "s|.*session.save_path = .*|session.save_path = '$(echo $REDIS_FQDN | grep -E '^\w+://' || echo tcp://$REDIS_FQDN):6379?auth=${REDIS_PASSWORD}'|" "$FILE"
         sed -i "s/session.sid_length = .*/session.sid_length = 64/" "$FILE"
         sed -i "s/session.use_strict_mode = .*/session.use_strict_mode = 1/" "$FILE"
     done
