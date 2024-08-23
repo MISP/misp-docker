ffe0b073542a1b121dc69b07a6d964f3862ab618
d-r-e <9976038+d-r-e@users.noreply.github.com>
2024-08-23 20:47:05 +0200

    Parametric configuration php fpm

--- core/files/entrypoint_fpm.sh
+++ core/files/entrypoint_fpm.sh
@@ -14,10 +14,10 @@ change_php_vars() {
     for FILE in /etc/php/*/fpm/php.ini
     do
         [[ -e $FILE ]] || break
-        sed -i "s/memory_limit = .*/memory_limit = $MEMORY_LIMIT/" "$FILE"
-        sed -i "s/max_execution_time = .*/max_execution_time = $MAX_EXECUTION_TIME/" "$FILE"
-        sed -i "s/upload_max_filesize = .*/upload_max_filesize = $UPLOAD_MAX_FILESIZE/" "$FILE"
-        sed -i "s/post_max_size = .*/post_max_size = $POST_MAX_SIZE/" "$FILE"
+        sed -i "s/memory_limit = .*/memory_limit = $PHP_MEMORY_LIMIT/" "$FILE"
+        sed -i "s/max_execution_time = .*/max_execution_time = $PHP_MAX_EXECUTION_TIME/" "$FILE"
+        sed -i "s/upload_max_filesize = .*/upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE/" "$FILE"
+        sed -i "s/post_max_size = .*/post_max_size = $PHP_POST_MAX_SIZE/" "$FILE"
         sed -i "s/session.save_handler = .*/session.save_handler = redis/" "$FILE"
         sed -i "s|.*session.save_path = .*|session.save_path = '$(echo $REDIS_FQDN | grep -E '^\w+://' || echo tcp://$REDIS_FQDN):6379${REDIS_PASSWORD:+?auth=${REDIS_PASSWORD}}'|" "$FILE"
         sed -i "s/session.sid_length = .*/session.sid_length = 64/" "$FILE"
