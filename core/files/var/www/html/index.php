1bfead39b36f25a3f2114a747b5d18054dc803a6
Guilherme Capilé <capile@tecnodz.com>
2024-11-14 11:51:04 -0300

    temporary response should not enable a successful healthcheck or cache #185

--- core/files/var/www/html/index.php
+++ core/files/var/www/html/index.php
@@ -1,3 +1,10 @@
+<?php
+$proto = (isset($_SERVER['SERVER_PROTOCOL']))?($_SERVER['SERVER_PROTOCOL']):('HTTP/1.1');
+header($proto.' 503 Service Unavailable', true);
+header('cache-control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+header('retry-after: 30');
+header('refresh: 30');
+?>
 <html>
 MISP is loading...
 </html>
