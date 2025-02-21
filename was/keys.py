42182f6b0c79c470bec101a98e3ed0cfa1149dd0
iteasy-ops-dev <iteasy.ops.dev@gmail.com>
2025-02-21 16:20:48 +0900

    Delete was/keys.py

--- was/keys.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-misp_url = 'https://misp-core'
-misp_key = 'F2X7J51IUCAtE9lsYQCv2y05dKqMNGbGIq1SmPug' # The MISP auth key can be found on the MISP web interface under the automation section
-misp_verifycert = False
-misp_client_cert = ''
-proofpoint_sp = '<proofpoint service principal>'  # Service Principal from TAP (https://threatinsight.proofpoint.com/<custID>/settings/connected-applications)
-proofpoint_secret = '<proofpoint secret>'
