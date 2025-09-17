8156c8725a362c8ea4998d6850b31edbcf2270e4
Stefano Ortolani <ortolanis@vmware.com>
2025-09-17 12:33:49 +0100

    Add multi-layer build and remove command in favour of env variables

--- guard/files/entrypoint.sh
+++ guard/files/entrypoint.sh
@@ -20,6 +20,6 @@ MISP_IP=$(getent hosts misp-core | awk '{print $1}')
 # replace runtime ip into config.json
 jq --arg ip "$MISP_IP" \
    '.instances.misp_container.ip = $ip' \
-   /srv/misp-guard/src/config.user.json > /srv/misp-guard/src/config.json
+   /config.json > /srv/misp-guard/src/config.json
 
-exec "$@"
+exec mitmdump -s mispguard.py -p ${GUARD_PORT:-8888} ${GUARD_VERBOSE} ${GUARD_SSL_INSECURE} --set config=config.json
